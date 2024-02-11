1d5c00d2d817d4be52707909ec8ddbb1b179be08
Kostin Ilya <ilyaspecial999@gmail.com>
2024-02-11 18:32:34 +0300

    1

--- server_modules/chat.js
+++ server_modules/chat.js
@@ -1,5 +1,3 @@
-const escape = require('mysql').escape;
-
 class Chat {
     constructor(cfx, id, name) {
         this.cfx = cfx;
@@ -27,7 +25,7 @@ class Chat {
         return this.cfx.db.executeFile('addmessage', {
             sender_id: sender_id,
             chat_id: this.id,
-            text: escape(text),
+            text: this.cfx.utils.mysql_escape(text),
             bundle: bundle ?? null
         })
         .then(() => {
