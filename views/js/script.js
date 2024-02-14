deb0318da660d3afdbefa72d446e387c8a33090f
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-02-14 10:14:26 +0300

    Update script.js

--- views/js/script.js
+++ views/js/script.js
@@ -49,3 +49,16 @@ class TemplateManager {
 }
 
 const templateManager = new TemplateManager();
+const menuPopup = new Popup({
+    closable: true,
+    html: templateManager.createHTML('menu'),
+    removeOnClose: false
+})
+
+function callPopup() {
+    menuPopup.open()   
+}
+
+
+
+
