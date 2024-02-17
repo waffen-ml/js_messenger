45f08faf4a1f3dd9e58beb039acc52fd38f6a44e
Kostin Ilya <ilyaspecial999@gmail.com>
2024-02-17 22:30:16 +0300

    1

--- views/js/ebank.js
+++ views/js/ebank.js
@@ -6,5 +6,20 @@ let form = new Form('ebank', null, () => {
 
 function openTransactions() {
 
+    fetch('/gettransactions')
+    .then(r => r.json())
+    .then(transactions => {
+
+        let popup = new Popup({
+            closable: true,
+            title: 'Недавние переводы',
+            html: templateManager.createHTML({transactions: transactions})
+        })
+
+        popup.addOption('Закрыть', () => true)
+
+        popup.open()
+
+    })
 
 }
