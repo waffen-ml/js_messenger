978319193d248c221b3afe2a630b9342ac917acf
Kostin Ilya <ilyaspecial999@gmail.com>
2024-02-18 14:56:59 +0300

    1

--- views/js/posts.js
+++ views/js/posts.js
@@ -1,7 +1,9 @@
 
 fetch('/auth')
-.then(r => r.json())
+.then(r => r.text())
 .then(r => {
+    console.log(r)
+    return
     let feed = new Feed(r, null, 
         document.querySelector('.feed'), 
         document.querySelector('main'))
