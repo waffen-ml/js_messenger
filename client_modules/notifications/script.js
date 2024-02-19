b3884a8382e39cfb16b62c2c94b8ebfa18724f8d
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-02-19 16:28:15 +0300

    1

--- client_modules/notifications/script.js
+++ client_modules/notifications/script.js
@@ -42,6 +42,7 @@ function showNotification(link, text, title, imagesrc) {
     }
 
     notificationWindow.style.display = "block";
+    notificationWindow.style.animation = ""
     notificationWindow.style.animation = "notification-open 400ms ease-in-out";
 
     hideTimeout = setTimeout(() => {
