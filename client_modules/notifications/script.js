0e4b38b16e1c45f3ec5b4e5456ec7116a57ca920
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-02-19 16:07:42 +0300

    1

--- client_modules/notifications/script.js
+++ client_modules/notifications/script.js
@@ -1,3 +1,58 @@
 const notificationWindow = document.querySelector('.notification')
+const textHolder = notificationWindow.querySelector('.text')
+const imageHolder = notificationWindow.querySelector('.avatar')
+const titleHolder = notificationWindow.querySelector('.title')
 
 
+
+function playNotificationSound() {
+
+}
+
+function hideNotification() {
+
+}
+
+function showNotification(link, text, title, imagesrc) {
+
+    if(textHolder) {
+        textHolder.style.display = 'block'
+        textHolder.textContent = text
+    } else {
+        textHolder.style.display = 'none'
+    }
+
+    if(titleHolder) {
+        titleHolder.style.display = 'block'
+        titleHolder.textContent = title
+    } else {
+        titleHolder.style.display = 'none'
+    }
+
+    if (imagesrc) {
+        imageHolder.src = imagesrc
+        imageHolder.style.display = 'block'
+    } else {
+        imageHolder.style.display = 'none'
+    }
+
+    if (link) {
+        notificationWindow.onclick = () => location.replace(link)
+        notificationWindow.style.cursor = 'pointer'
+    } else {
+        notificationWindow.onclick = () => {}
+        notificationWindow.style.cursor = 'default'
+    }
+
+    notificationWindow.style.display = "block";
+    notificationWindow.style.animation = "notification-open 400ms ease-in-out";
+
+    
+
+    setTimeout(() => {
+        hideNotification()
+    }, 3000)
+
+
+
+}
