8717ad0ad87324f358729651f839251057851f48
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-02-21 10:47:53 +0300

    1

---
+++ client_modules/contextwindow/script.js
@@ -0,0 +1,52 @@
+const button = document.querySelector('#hi')
+
+function cwTest() {
+    let w = {}
+    ['hey', 'hi', 'vovapidr'].forEach(msg => {
+        w[msg] = () => console.log(msg)
+    })
+
+    makeButtonsCW(w, {
+        pos: {
+            top: button.clientTop,
+            left: button.clientLeft
+        }
+    })
+    
+}
+
+function makeButtonsCW(buttons, pos) {
+    pos ??= {}
+
+    let cw = new ContextWindow({
+        html: templateManager.createHTML('buttons-cw', {
+            labels: Object.keys(buttons)
+        })
+    })
+
+    return cw
+
+}
+
+class ContextWindow {
+
+    constructor(options) {
+
+        this.window = templateManager.createElement({
+            html: templateManager.createHTML('context-window')
+        })
+        this.parent = options.parent ?? document.body
+
+        this.parent.appendChild(this.window)
+
+        this.setPosition(options.pos ?? {})
+    }
+
+    setPosition(pos) {
+        ['top', 'left', 'right', 'bottom'].forEach(dir => {
+            if (pos[dir])
+                this.window.style[dir] = pos[dir] + 'px'
+        })
+    }
+
+}
