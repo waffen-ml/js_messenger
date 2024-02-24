d6a25fd9c40a4d974eb6812a70e48744be6b9ef8
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-02-24 10:22:21 +0000

    1

--- server_modules/notifications.js
+++ server_modules/notifications.js
@@ -36,6 +36,7 @@ class Notifications {
         const payload = JSON.stringify(data)
 
         this.cfx.core.sessionStorage.all((sessions) => {
+            console.log(sessions)
             let subs = sessions.filter(s => s.user && s.user.id == userid).filter(s => s.notificationSubscription)
                 .map(s => s.notificationSubscription)
             
