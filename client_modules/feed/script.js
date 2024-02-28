ba708fd0a40a4675d302ac805da7cecb89df30b0
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-02-28 14:42:27 +0300

    1

--- client_modules/feed/script.js
+++ client_modules/feed/script.js
@@ -28,17 +28,17 @@ class Post {
 
         return this.feed.loadComments(this.id, this.commentLoadStart, commentLoadBatchSize + 1)
         .then(comments => {
-            console.log(comments.reverse())
+            comments.reverse() // first id is min
 
             if(!comments.length)
                 return
 
             if (comments.length > commentLoadBatchSize) {
                 this.toggleLoadCommentsButton(true)
-                comments.pop()
+                comments.shift()
             }
-            this.commentLoadStart = comments[comments.length - 1].id - 1
-            comments.reverse().forEach(c => {
+            this.commentLoadStart = comments[0].id - 1
+            comments.forEach(c => {
                 c.datetime = new Date(c.datetime)
                 this.addComment(c, true)
             })
