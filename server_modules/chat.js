93ead31566875762ccb29e2e7afdd8a1581bee3c
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-03-01 12:10:50 +0300

    1

--- server_modules/chat.js
+++ server_modules/chat.js
@@ -1,3 +1,5 @@
+
+
 class Chat {
     constructor(cfx, id, name) {
         this.cfx = cfx;
@@ -174,12 +176,12 @@ class ChatSystem {
         })
     }
 
-    accessChat(user, chatid) {
+    accessChat(userid, chatid) {
         return this.getChat(chatid)
         .then(chat => {
             if (!chat)
                 throw new Error('Chat was not found')
-            return chat.containsUser(user.id)
+            return chat.containsUser(user)
             .then(r => {
                 if (!r) throw new Error('User is not in the chat')
                 return chat
@@ -340,8 +342,7 @@ exports.init = (cfx) => {
     }, true)
 
     cfx.core.safeRender('/chat', (user, req, res) => {
-        throw Error('hey')
-        return cfx.chats.accessChat(user, req.query.id)
+        return cfx.chats.accessChat(user.id, req.query.id)
         .then(chat => {
             chat.makeMessageRead([user.id])
             return {
@@ -351,14 +352,14 @@ exports.init = (cfx) => {
     }, true)
 
     cfx.core.safeGet('/getchatinfo', (user, req, res) => {
-        return cfx.chats.accessChat(user, req.query.id)
+        return cfx.chats.accessChat(user.id, req.query.id)
         .then(chat => {
             return chat.getInfo()
         })
     }, false)
 
     cfx.core.safeGet('/getmessages', (user, req, res) => {
-        return cfx.chats.accessChat(user, req.query.chatid)
+        return cfx.chats.accessChat(user.id, req.query.chatid)
         .then(chat => {
             let start = parseInt(req.query.start)
             let count = parseInt(req.query.count)
