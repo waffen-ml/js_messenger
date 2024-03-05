dd8072ae91db3e772e2550d87b693ff6b6e88c3d
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-03-05 22:26:59 +0300

    1

--- server_modules/files.js
+++ server_modules/files.js
@@ -60,7 +60,7 @@ exports.init = (cfx) => {
             let length = Buffer.byteLength(buffer)
             
             res.status(304)
-            //res.setHeader('Content-Disposition', 'inline; filename=' + encodeURI(file.name))
+            res.setHeader('Content-Disposition', 'inline; filename=' + encodeURI(file.name))
             res.setHeader('Content-Length', length)
             res.setHeader('Content-Range', `bytes 0-${length}/${length}`)
             res.setHeader('Accept-Ranges', 'bytes')
