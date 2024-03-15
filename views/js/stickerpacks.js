79de4ca54e8737e998aeeb1f8ee6ca4752d4bb4e
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-03-15 12:40:22 +0300

    1

--- views/js/stickerpacks.js
+++ views/js/stickerpacks.js
@@ -2,7 +2,24 @@ const stickerpackHolder = document.querySelector('.stickerpacks')
 
 
 function openStickerpack(spack) {
-    alert('hey')
+    let popup = new Popup({
+        html: '<div class="stickers"></div>',
+        className: 'inspect-stickerpack',
+        closable: true
+    })
+
+    if (!spack.available) {
+        popup.addOption(`Купить (${spack.price} EBL)`, () => {
+            alert('buy!')
+        })
+    }
+
+    let stickerHolder = popup.querySelector('stickers')
+
+    for(let i = 0; i < spack.count; i++)
+        stickerHolder.innerHTML += `<img class="sticker" src="/public/stickers/${spack.tag}/${i}.png">`
+
+    popup.open()
 }
 
 
@@ -10,10 +27,8 @@ fetch('/getallstickerpacks')
 .then(r => r.json())
 .then(spacks => {
     spacks.forEach(spack => {
-
         let element = templateManager.createElement('stickerpack', spack)
-
+        element.addEventListener('click', () => openStickerpack(spack))
         stickerpackHolder.appendChild(element)
-
     })
 })
