1aa6de60a1e1225ab6320904f62481d579214725
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-03-17 13:07:05 +0300

    1

--- views/js/settings.js
+++ views/js/settings.js
@@ -1,7 +1,7 @@
 const publicVapidKey = 'BFz5DJhb3Fxpj5UB855BnYqXV6HCi2_UJyYGsgEFZRBAGCrm9XThi18-BFxb_cv7lgcrH0Lguj3J6SWfv3E02E8'
 const avatarImg = document.querySelector('.avatar')
 
-function send() {
+async function send() {
     const register = await navigator.serviceWorker.register('/public/worker.js')
     const subscription = await register.pushManager.subscribe({
         userVisibleOnly: true,
