d15450abb24943235ed83ca580e61435c7fa467f
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-03-17 14:13:14 +0300

    1

--- public/worker.js
+++ public/worker.js
@@ -3,6 +3,20 @@ console.log('Service worker loaded!')
 self.addEventListener('push', e => {
     let data = e.data.json()
     
+    //let notification = new Notification('CoffeeTox', {
+    //    body: data.body ?? 'HEY',
+    //    icon: data.icon ?? 'https://coffeetox.ru/public/coffee.png',
+    //    tag: data.tag,
+    //    silent: false,
+    //    dir: 'ltr',
+    //})
+    //notification.addEventListener('notificationclick', (event) => {
+    //    alert('hey')
+    //    //if(data.link) {
+    //    //    event.notification.close()
+    //    //    clients.openWindow("https://youtu.be/PAvHeRGZ_lA")
+    //    //}
+    //})
     let notification = new Notification('CoffeeTox', {
         body: 'HEY',
         icon: data.icon ?? 'https://coffeetox.ru/public/coffee.png',
@@ -11,13 +25,5 @@ self.addEventListener('push', e => {
         dir: 'ltr',
     })
 
-    notification.addEventListener('notificationclick', (event) => {
-        alert('hey')
-        //if(data.link) {
-        //    event.notification.close()
-        //    clients.openWindow("https://youtu.be/PAvHeRGZ_lA")
-        //}
-    })
-
-    self.registration.showNotification(notification)
+    self.registration.showNotification('hey', {body: 'hey'})
 })
