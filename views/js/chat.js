0c5e6cdc7dc52d228a413645e98eafc352975f77
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-03-17 13:27:54 +0300

    1

--- views/js/chat.js
+++ views/js/chat.js
@@ -374,6 +374,7 @@ class Chat {
         socket.emit('join-chat', chatid);
 
         socket.on('message', msg => {
+            console.log(msg)
             this.messages.addMessages([msg], true, false, true)
             fetch('/readmessages?id=' + this.chatid)
         })
