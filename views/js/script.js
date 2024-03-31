61ac029973224b80d75e455d449794f7d74a0a61
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-03-31 13:09:34 +0300

    1

--- views/js/script.js
+++ views/js/script.js
@@ -41,7 +41,7 @@ class TemplateManager {
     createElement(id, data) {
         let div = document.createElement('div')
         div.innerHTML = this.createHTML(id, data)
-        let elements = div.querySelectorAll('*')
+        let elements = div.children
         console.log(elements)
         return elements.length > 1? elements : elements[0]
     }
