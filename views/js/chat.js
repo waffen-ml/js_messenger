d7b64f31568ab9ab9948316de026ed0fa8a6a2ab
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-03-31 13:28:35 +0300

    1

--- views/js/chat.js
+++ views/js/chat.js
@@ -115,10 +115,10 @@ class ChatInspector {
         this.popup.content.querySelectorAll('.tab')
             .forEach(tab => tab.classList.remove('active'))
         this.popup.content.querySelectorAll('.tab#' + id).classList.add('active')
-        
+        console.log('hey')
         this.popup.content.querySelectorAll('.controls a')
             .forEach(a => a.classList.remove('chosen'))
-        this.popup.content.querySelector('.controls a#show-' + id).classList.add('chosen')
+        this.popup.content.querySelector('.controls #show-' + id).classList.add('chosen')
     }
 
     toggleUnsavedHandling(state) {
