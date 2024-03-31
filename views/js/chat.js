8a16114f0c75d8b489aa53d8b4c9528178cc6140
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-03-31 20:24:09 +0300

    1

--- views/js/chat.js
+++ views/js/chat.js
@@ -995,6 +995,8 @@ class Chat {
     applyInfoChanges(changes) {
         let fd = new FormData()
 
+        console.log(changes)
+
         if(changes.name === null)
             fd.append('deleteName', 1)
         else if(changes.name)
