cc86151677898faf58c3567832025f01d9a10094
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-04-07 18:40:08 +0300

    1

--- client_modules/call/script.js
+++ client_modules/call/script.js
@@ -1,4 +1,4 @@
-const Call {
+const Hey {
     constructor() {
         this.callInterface = document.querySelector('.call-interface')
         this.mainBar = this.callInterface.querySelector('.main-bar')
@@ -24,4 +24,4 @@ const Call {
 
 }
 
-let call = new Call();
+let call = new Hey();
