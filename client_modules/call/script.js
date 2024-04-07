5ab2451786c7db66145f9f372aa0ada9a27f05fc
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-04-07 21:24:56 +0300

    1

--- client_modules/call/script.js
+++ client_modules/call/script.js
@@ -1,21 +1,14 @@
 class Call {
     async constructor(callid, myid) {
         this.id = callid
+        this.myid = myid
         this.callInterface = document.querySelector('.call-interface')
         this.mainBar = this.callInterface.querySelector('.main-bar')
         this.memberList = this.callInterface.querySelector('.member-list')
 
-        this.members = await fetch('/getcallmembers?id=' + this.id).then(r => r.json())
+        await this.getMyStream()
+        await this.getMembers()
 
-        if(this.members.error)
-            throw Error('Cannot get members')
-        else if(myid in this.members)
-            throw Error('The user is already in the call')
-
-        this.members.forEach(m => {
-            m.muted = false
-            m.volume = 100
-        })
 
         socket.emit('join_call', this.id)
 
@@ -31,6 +24,82 @@ class Call {
             this.isCompact = !this.isCompact
             this.toggleCompact(this.isCompact)
         }
+
+    }
+
+    async getMyStream() {
+        this.myStream = await navigator.mediaDevices.getUserMedia({audio: true, video: false}).catch(() => null)
+
+        if(!this.myStream)
+            throw Error('CANNOT_GET_STREAM')
+    }
+
+    async getMembers() {
+        this.members = await fetch('/getcallmembers?id=' + this.id).then(r => r.json())
+
+        if(this.members.error)
+            throw Error('CANNOT_GET_MEMBERS')
+        else if(myid in this.members)
+            throw Error('USER_IN_THE_CALL')
+
+        this.members.forEach(m => {
+            m.muted = false
+            m.volume = 100
+        })
+    }
+
+    setupPeer() {
+        this.peer = new Peer(undefined, {
+            host: '/',
+            port: '3001',
+            secure: true
+        })
+
+        this.peer.on('open', peerid => {
+            this.peer.peerid = peerid
+            fetch('/joincall?chatid=' + this.chatid + '&peerid=' + peerid)
+            .then(r => r.json())
+            .then(r => {
+                if (!r.success) {
+                    alert('Не удалось подключиться.')
+                    this.destroyPeer()
+                    return
+                }
+                this.interface.updateHeaderCallButtons(true)
+                this.interface.showControls()
+            })
+        })
+    
+        this.peer.on('call', call => {
+            call.answer(this.stream)
+            call.on('stream', userStream => {
+                console.log('connected to me: ' + call.peer)
+                this.connectToMember(call, userStream)
+            })
+        })
+    }
+
+
+    updateMemberStream(toggle, volume) {
+
+
+    }
+
+    setupSocket() {
+        socket.on('user_joined_chat', user => {
+
+        })
+
+        socket.on('user_left_chat', user => {
+
+        })
+    }
+
+    disconnectFromMember(userid) {
+
+    }
+
+    leave() {
 
     }
 
