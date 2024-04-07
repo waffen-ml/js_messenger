56554b28a1c08bdc71b698c45681d016179534f6
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-04-07 16:53:16 +0300

    1

--- client_modules/notifications/script.js
+++ client_modules/notifications/script.js
@@ -56,7 +56,7 @@ socket.on('message', async (msg) => {
     let chatInfo = await fetch('/getchatinfo?id=' + msg.chat_id).then(r => r.json())
 
     showFocusNotification({
-        text: utils.getMessageContentPreview(msg.content, true),
+        text: utils.getMessageContentPreview(msg, true),
         title: chatInfo.name ?? utils.generateChatName(chatInfo.members, window.me),
         link: '/chat?id=' + msg.chat_id,
         imagesrc: '/getchatavatar?id=' + msg.chat_id
