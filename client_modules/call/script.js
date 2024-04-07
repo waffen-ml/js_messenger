93d1900e8201b55060735e551dc0d9f63c5f345b
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-04-07 21:12:06 +0300

    1

--- client_modules/call/script.js
+++ client_modules/call/script.js
@@ -1,8 +1,28 @@
 class Call {
-    constructor() {
+    async constructor(callid, myid) {
+        this.id = callid
         this.callInterface = document.querySelector('.call-interface')
         this.mainBar = this.callInterface.querySelector('.main-bar')
         this.memberList = this.callInterface.querySelector('.member-list')
+
+        this.members = await fetch('/getcallmembers?id=' + this.id).then(r => r.json())
+
+        if(this.members.error)
+            throw Error('Cannot get members')
+        else if(myid in this.members)
+            throw Error('The user is already in the call')
+
+        this.members.forEach(m => {
+            m.muted = false
+            m.volume = 100
+        })
+
+        socket.emit('join_call', this.id)
+
+        
+
+
+
         this.isCompact = true
 
         this.toggleCompact(true)
