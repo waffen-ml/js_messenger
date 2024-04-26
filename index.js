0bb67cdfcb3997b2cb3c640928940d3423cc53b7
waffen-ml <76839089+waffen-ml@users.noreply.github.com>
2024-04-26 20:42:37 +0300

    1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const fs = require('fs')
 const pug = require('pug')
 
-const keyPath = __dirname + `/../sslcert/ca-key.pem`
+const keyPath = __dirname + `/../sslcert/key.pem`
 const certPath = __dirname + `/../sslcert/cert.pem`
 
 
